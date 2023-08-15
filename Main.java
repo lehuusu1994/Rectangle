df49144f33e060f5f4308c11786912ad707cb546
unknown <leesu031994@gmail.com>
2023-08-15 10:10:22 +0700

    hinh_chu_nhat

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.zip.CheckedInputStream;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width");
+        double width =scanner.nextDouble();
+        System.out.println("Enter the height");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+
+}
